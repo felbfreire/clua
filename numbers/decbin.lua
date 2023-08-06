6b55b4a8d7eafcf15908ac650fa86c9ddb6787b0
felbfreire <felbfreire@proton.me>
2023-08-06 17:33:23 -0300

    decimal to binary conversion (lua)

---
+++ numbers/decbin.lua
@@ -0,0 +1,17 @@
+decimal = {0, 0, 0};
+
+for dindex in pairs(decimal) do
+	io.write("input a number: ")
+	d = io.read("n")
+	decimal[dindex] = d
+end
+
+for dindex in pairs(decimal) do
+
+	io.write(decimal[dindex] .. " in binary is: ")
+	while decimal[dindex] > 0.9 do
+		io.write(math.floor(decimal[dindex] % 2))
+		decimal[dindex] = decimal[dindex] / 2
+	end
+end
+
